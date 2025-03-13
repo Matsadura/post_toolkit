86b41e8889da09a570187555e449fa3001accd4c
Matsadura <zidanditoo1@gmail.com>
2025-03-13 15:57:11 +0000

    [ADD] Install script, lock autostart config

---
+++ install_zdn.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+printf "Enter the device's MAC address: "
+read MAC
+echo $MAC
+
+wget -L https://raw.githubusercontent.com/Matsadura/post_toolkit/refs/heads/main/lock.py
+wget -L https://raw.githubusercontent.com/Matsadura/post_toolkit/refs/heads/main/bt_on.bash
+https://raw.githubusercontent.com/Matsadura/post_toolkit/refs/heads/main/bt_off.bash
+
+sed -i "s/MAC_HERE/$MAC/g" bt_on.bash
+sed -i "s/MAC_HERE/$MAC/g" bt_off.bash
+sed -i "s/USER_HERE/$USER/g" bt_on.bash
+sed -i "s/USER_HERE/$USER/g" python3.desktop
+
+chmod +x bt_on.bash bt_off.bash lock.py
+
+mkdir -p /home/$USER/.local/bin
+cp bt_on.bash /home/$USER/.local/bin/bton
+cp bt_off.bash /home/$USER/.local/bin/btoff
+cp lock.py /home/$USER/.local/bin/lock
+cp python3.desktop /home/$USER/.config/autostart/
+
+pip3 install pynput >/dev/null 2>&1
